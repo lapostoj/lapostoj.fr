035d27450c50414539c4f8734f0ff5c84c8bd46a
lapostoj <jerome.lapostolet@gmail.com>
2020-03-29 22:32:26 +0100

    Add anchors on headers with gatsby-remark-autolink-headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,6 +97,7 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          'gatsby-remark-autolink-headers',
           {
             resolve: 'gatsby-remark-external-links',
             options: {
