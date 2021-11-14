379beff2da950a940355320c295bc56bed19de84
Jerome Lapostolet <jerome.lapostolet@gmail.com>
2021-11-15 00:12:58 +0100

    Update page not found

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -11,7 +11,7 @@ const NotFound = ({ data }) => (
       <div className="content">
         <div className="content__inner">
           <div className="page">
-            <h1 className="page__title">NOT FOUND</h1>
+            <h1 className="page__title">Page Not Found</h1>
             <div className="page__body">
               <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
             </div>
